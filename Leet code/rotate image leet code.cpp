ec6872e9e72fd06e4afa28c5968d651de9c331e1
deep04102004 <dkb6865@gmail.com>
2025-05-25 22:35:29 +0530

    Leet Code

---
+++ Leet code/rotate image leet code.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<vector<int>> rotate(vector<vector<int>>&matrix)
+{
+	int n=matrix.size();
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<i;j++)
+		{
+			swap(matrix[i][j],matrix[j][i]);
+		}
+	}
+	
+	for(auto&num:matrix)
+	{
+		reverse(num.begin(),num.end());
+	}
+	return matrix;
+}
+
+int main()
+{
+	vector<vector<int>> matrix={{1,2,3},{4,5,6},{7,8,9}};
+	vector<vector<int>>ans = rotate(matrix);
+	for(vector<int>&n:ans)
+	{
+		for(int&v:n)
+		{
+			cout<<v<<" ";
+		}
+		cout<<endl;
+	}
+	
+	
+}

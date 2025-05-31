8ea2e878b149290a6d0da538d69788a65befdd58
deep04102004 <dkb6865@gmail.com>
2025-05-31 20:27:54 +0530

    New leetcode 26

---
+++ Leet_code/leetcode_26.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0; // Handle empty array case
+
+        int j = 0; // Pointer for the position of unique elements
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[j] != nums[i]) {
+                nums[++j] = nums[i]; // Move the unique element to the next position
+            }
+        }
+        return j + 1; // Return the length of the array with unique elements
+    }
+int main(){
+vector<int>nums={1,1,2};
+cout<<removeDuplicates(nums);
+}

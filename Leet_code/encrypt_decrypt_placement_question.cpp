991f3409802075c3175db587411623cdf037c7b8
deep04102004 <dkb6865@gmail.com>
2025-06-19 20:52:44 +0530

    placement Question

---
+++ Leet_code/encrypt_decrypt_placement_question.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>distinctprime(int num){
+    unordered_set<int>primes;
+    while(num%2==0){
+         primes.insert(2);
+         num/=2;
+    }
+    for(int i=3;i*i<=num;i+=2){
+        if(num%i==0){
+            primes.insert(i);
+            num/=i;
+        }
+    }
+    if(num>2){
+        primes.insert(num);
+    }
+   return vector<int>(primes.begin(),primes.end());
+}
+int main(){
+    int n,multiply=1;
+    cout<<"Enter the number of Elements to Decrypt: ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"Enter Elements in array ";
+    for(int&a:arr){
+        cin>>a;
+    }
+    for(int&b:arr){
+        multiply*=b;
+    }
+    vector<int> decrypt=distinctprime(multiply);
+    cout<<"Decrypt Data is:";
+    for(int&c:decrypt){
+        cout<<c<<" ";
+    }
+
+
+}
